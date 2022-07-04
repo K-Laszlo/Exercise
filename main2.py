3af86851a05fac52b9f343e2abd0068fe36fe78f
Szabo Kristof <kristof89szabo@gmail.com>
2022-07-04 11:51:15 +0200

    for loop

--- main2.py
+++ main2.py
@@ -7,12 +7,12 @@ car3 = 'Audi'
 # print(cars[0])
 # print(cars[1])
 # print(cars[2])
-#
+
 # list_mixed = [2, 3, False, 'Dog']
 # list_homogeneous = [2, 3, 4, 5, 6, 7]
 
 
-cars = ['Tesla', 'Trabant', 'Audi', 'Bmw', 'Ford', 'Ferrari']
+# cars = ['Tesla', 'Trabant', 'Audi', 'Bmw', 'Ford', 'Ferrari']
 # sliced_cars = cars[1:4]  # excluded a 2. szám
 # print(sliced_cars)
 
@@ -26,10 +26,41 @@ cars = ['Tesla', 'Trabant', 'Audi', 'Bmw', 'Ford', 'Ferrari']
 # sliced_cars5 = cars[:-3]
 # print(sliced_cars5)
 
-sliced_cars6 = cars[:]
-print(sliced_cars6)
+# sliced_cars6 = cars[:]
+# print(sliced_cars6)
+#
+# my_str = 'Little Dog'
+# print('darab', len(my_str))
+# print('index', len(my_str)-1)
+# print(my_str[1:3])
 
-my_str = 'Little Dog'
-print('darab', len(my_str))
-print('index', len(my_str)-1)
-print(my_str[1:3])
+
+# Forloop
+
+cars = ['Tesla', 'Trabant', 'Audi', 'Bmw', 'Ford', 'Ferrari']
+
+# for car in cars:
+#     print(car + " ", end='')
+
+# for car in cars:
+#     print(car.upper())
+
+# for car in cars:
+#     if car == 'Tesla':
+#         print('Nyeeeeehh')
+#     else:
+#         print('Cool car')
+
+# number_list = range(10)  # [0,1,2,3,4,5,6,7,8,9]
+#
+# for num in number_list:
+#     if num % 2 == 0 and num != 0:
+#         print(str(num) + ' ', end='')
+
+# num_list = range(2, 20)
+# for num in num_list:
+#     print(num)
+
+num_list = range(2, 20, 3)
+for num in num_list:
+    print(num)
