26adb2d4cebbbbfefcacd98d68b98011308d06fc
Szabo Kristof <kristof89szabo@gmail.com>
2022-07-04 10:02:08 +0200

    primitives numbers string

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-print('Test master branch protection')
-print('Valami')
-print('Valami5')
-print('valami7')
+# Komment  CTRL + /
+
+# print('Hello Class')
+# print('Hello Class')
+# print('Hello Class')
+# print('Hello Class')
+
+"""
+print('Hello Class')
+"""
+# Numbers
+number1: int = 10  # int (integer)
+number2: float = 14.23  # float
+print(type(number1))
+print(number1)
+
+# number3 = 32.43e17
+# number4 = .876j
+
+# String
+name = 'kriszTina'
+hair_color = 'black'
+print(type(name))
+
+full_name = name + ' ' + hair_color
+print(full_name)
+
+# print(name.capitalize())
+# print(name.lower())
+# print(name.upper())
+print(name.startswith('K'))
+print(name.startswith('k'))
+print(name.endswith('a'))
