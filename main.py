9ffc09174728a099132cae2d43edd5e35318b0f9
Szabo Kristof <kristof89szabo@gmail.com>
2022-07-04 10:20:40 +0200

    input, casting, boolean, working with numbers

--- main.py
+++ main.py
@@ -11,23 +11,64 @@ print('Hello Class')
 # Numbers
 number1: int = 10  # int (integer)
 number2: float = 14.23  # float
-print(type(number1))
-print(number1)
+# print(type(number1))
+# print(number1)
 
 # number3 = 32.43e17
 # number4 = .876j
 
 # String
-name = 'kriszTina'
-hair_color = 'black'
-print(type(name))
-
-full_name = name + ' ' + hair_color
-print(full_name)
+# name = 'kriszTina'
+# hair_color = 'black'
+# print(type(name))
+#
+# full_name = name + ' ' + hair_color
+# print(full_name)
 
 # print(name.capitalize())
 # print(name.lower())
 # print(name.upper())
-print(name.startswith('K'))
-print(name.startswith('k'))
-print(name.endswith('a'))
+# print(name.startswith('K'))
+# print(name.startswith('k'))
+# print(name.endswith('a'))
+
+
+# working with numbers   + - * /  //  %
+
+adding = 10 + 5
+print(adding)
+
+sub = 10 - 5
+print(sub)
+
+mult = 10 * 5
+print(mult)
+
+div = 10 / 3
+print(div)
+
+div = 10 // 3
+print(div)
+
+mod = 10 % 3
+print(mod)
+
+check = (4 + 5) * 2
+print(check)
+
+# boolean  True/False
+print(10 <= 10)
+print(10 <= 9)
+is_adult = True
+is_teenager = False
+
+variable1 = '1'
+variable2 = '2'
+print(variable1+variable2)
+print(int(variable1)+int(variable2))
+
+#input
+user_age = input('Give me your age')
+int_age = int(user_age)
+print(int_age)
+print(type(int_age))
