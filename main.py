e1aba655dc9b058734b5f98ce1ae21b3c364003a
Szabo Kristof <kristof89szabo@gmail.com>
2022-07-04 10:49:51 +0200

    if elif else

--- main.py
+++ main.py
@@ -35,40 +35,70 @@ number2: float = 14.23  # float
 
 # working with numbers   + - * /  //  %
 
-adding = 10 + 5
-print(adding)
-
-sub = 10 - 5
-print(sub)
-
-mult = 10 * 5
-print(mult)
-
-div = 10 / 3
-print(div)
-
-div = 10 // 3
-print(div)
-
-mod = 10 % 3
-print(mod)
-
-check = (4 + 5) * 2
-print(check)
+# adding = 10 + 5
+# print(adding)
+#
+# sub = 10 - 5
+# print(sub)
+#
+# mult = 10 * 5
+# print(mult)
+#
+# div = 10 / 3
+# print(div)
+#
+# div = 10 // 3
+# print(div)
+#
+# mod = 10 % 3
+# print(mod)
+#
+# check = (4 + 5) * 2
+# print(check)
 
 # boolean  True/False
-print(10 <= 10)
-print(10 <= 9)
-is_adult = True
-is_teenager = False
+# print(10 <= 10)
+# print(10 <= 9)
+# is_adult = True
+# is_teenager = False
 
-variable1 = '1'
-variable2 = '2'
-print(variable1+variable2)
-print(int(variable1)+int(variable2))
+# variable1 = '1'
+# variable2 = '2'
+# print(variable1 + variable2)
+# print(int(variable1) + int(variable2))
 
-#input
-user_age = input('Give me your age')
-int_age = int(user_age)
-print(int_age)
-print(type(int_age))
+# input
+# user_age = input('Give me your age')
+# int_age = int(user_age)
+# print(int_age)
+# print(type(int_age))
+
+# if statement
+
+my_number = 25
+my_number2 = 10
+
+# if my_number == 6:
+#     print('Correct')
+#
+# print('Application done')
+
+# if my_number == 6:  # HA
+#     print('Correct')
+# else:  # egyébként
+#     print('Wrong number')
+
+if my_number == 5:  # HA
+    print('Correct')
+elif my_number == 6:  # Azonban HA
+    print('Number 6')
+elif my_number == 7:
+    print('Number 7')  # Azonban HA
+elif my_number == 8 or my_number2 == 9:
+    print('DEVOPS')
+elif 20 <= my_number <= 45:
+    print('bigger 20')
+else:  # egyébként
+    print('Wrong number')
+
+print('Application done')
