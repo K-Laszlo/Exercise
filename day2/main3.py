93bf8531e5907d1e1f0937dae84cce1964bf45bc
Szabo Kristof <kristof89szabo@gmail.com>
2022-07-05 11:01:02 +0200

    build-on functions

--- day2/main3.py
+++ day2/main3.py
@@ -13,8 +13,52 @@ age2 = 14
 # john_greeting = greeting.format(name, age3)
 # print(john_greeting)
 
-name = 'Krisztina'
-age4 = 18
-greeting = 'Hello {n}, you are {a} years old'
-john_greeting = greeting.format(n=name, a=age4)
-print(john_greeting)
+# name = 'Krisztina'
+# age4 = 18
+# greeting = 'Hello {n}, you are {a} years old'
+# john_greeting = greeting.format(n=name, a=age4)
+# print(john_greeting)
+
+# build in functions
+
+word = 'hEllo'
+# print(word.upper())
+# print(word.lower())
+# print(word.capitalize())
+# print(word.startswith('h'))
+# print(word.startswith('H'))
+# print(word.endswith('o'))
+# print(word.endswith('O'))
+
+my_number_list = [3, 2, 2, 50, -20, -30]
+# a = min(my_number_list)
+# print(a)
+#
+# print(max(my_number_list))
+#
+# print(sum(my_number_list))
+
+# my_number_list.sort()
+# print(my_number_list)
+#
+# my_str_list = ['z', 'a', 'b', 'c']
+# my_str_list.sort()
+# print(my_str_list)
+
+# my_number_list.reverse()
+# print(my_number_list)
+#
+# my_number_list.append(999)
+# print(my_number_list)
+#
+# my_number_list.insert(0, 999)  # elso parameter az index
+# print(my_number_list)
+#
+# print(len(my_number_list))
+
+# print(my_number_list)
+# my_number_list.clear()  # Ures listank lesz a clear utan
+# print(my_number_list)
+
+print(49 not in my_number_list)
+print(50 in my_number_list)
