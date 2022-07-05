f5e48390d6a1cccf9cd603333dc1540094d8d682
Szabo Kristof <kristof89szabo@gmail.com>
2022-07-05 14:45:23 +0200

    set

---
+++ day2/main6.py
@@ -0,0 +1,18 @@
+number_list = [1, 54, 23, 51, 2, 3, 2, 54, 23]  # list
+
+# number_set = {1, 54, 23, 51, 2, 3, 2, 54, 23}  # {} kozott, nincs duplikacio
+# print(number_set)
+
+# number_set2 = {1, 2, 3}  # nincs indexeles
+#
+# number_set2.add(4)
+# print(number_set2)
+# number_set2.add(4)  # nem kerul bele nincs duplikacio
+# print(number_set2)
+# number_set2.remove(2)  # erteket rakunk bele nem indexet
+# print(number_set2)
+
+letter_set = {'A', 'C', 'c', 'B,', 'Z', 'b', 'SZ', 'k'} # Nem tartja a sorrendet!!!!!!!
+print(letter_set)
+
+
