e914d9321d21945ba967e4214c25fd303aef1548
Szabo Kristof <kristof89szabo@gmail.com>
2022-07-06 10:19:34 +0200

    dictionary

--- day3/main7.py
+++ day3/main7.py
@@ -18,10 +18,11 @@
 # print(friends2)
 
 # Dictionary
-person = {'name': 'Tommy',
-          'age': 25,
-          'address': 'USA',
-          }
+person = {
+    'name': 'Tommy',
+    'age': 25,
+    'address': 'USA',
+}
 
 # new_dict = {  # ne igy hozzunk letre ures dictionaryt
 #     '': ''
@@ -36,3 +37,46 @@ person = {'name': 'Tommy',
 
 # print(person.keys())
 # print(person.values())
+# Add new key value pair
+# person['street'] = 'Hos utca'
+# print(person)
+#
+# my_street = person.pop('street')  # return value
+# print(person)
+# print(my_street)
+#
+# person['street'] = my_street
+# print(person)
+#
+# # Delete key value pair
+# del person['street']
+# print(person)
+
+# print(person.get('age'))
+# person['age2'] = 18
+# print(person.get('age'))
+# print(person)
+# person.update({'age': 5})
+# print(person)
+
+# for k in person:
+#     print(f'Keys: {k}')
+#
+# for k in person.keys():
+#     print(f'Keys: {k}')
+#
+# for v in person.values():
+#     print(f'Values: {v}')
+
+for k, v in person.items():  # key value iteration
+    print(f'Keys: {k} Values: {v}')
+
+for k in person.keys():  # key iteration  but we can ask out the value from person.
+    print(f'Keys: {k} Values: {person[k]}')
+#
+# person.clear()  # Dictionary megmarad de ures lesz.
+# print(person)
+
+my_list = [1, 2, 3]
+person['music'] = my_list
+print(person['music'][1])
