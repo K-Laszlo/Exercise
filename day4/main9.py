8bced89ca67ef62eb612d855992d80d7331fa2ed
Szabo Kristof <kristof89szabo@gmail.com>
2022-07-07 10:45:50 +0200

    csv

--- day4/main9.py
+++ day4/main9.py
@@ -12,8 +12,8 @@
 #     my_content2 = my_text2.read()
 #     print(my_content2)
 #     # Nem kell lezarni
-
-user_input = input('Neved?')
+#
+# user_input = input('Neved?')
 
 # with open('data.txt', 'w', encoding='UTF-8') as file:
 #     file.write(user_input) # Torli a file tartalmat majd beleirja az uj adatot
@@ -23,9 +23,44 @@ user_input = input('Neved?')
 #     file.write(user_input)
 
 
-try:
-    with open('data2.txt', 'r', encoding='UTF-8') as file:
-        content = file.read()
-        print(content)
-except FileNotFoundError:
-    print('Could not find this file')
+# try:
+#     with open('data2.txt', 'r', encoding='UTF-8') as file:
+#         content = file.read()
+#         print(content)
+# except FileNotFoundError:
+#     print('Could not find this file')
+
+#
+# with open('data.txt', 'r', encoding='UTF-8') as file:
+#     content = file.readline() # Egy sort olvas
+#     print(content, end='')
+#     print(type(content))
+
+# with open('data.txt', 'r', encoding='UTF-8') as file:
+#     content = file.readline()
+#     while content:
+#         print(content, end='')
+#         content = file.readline()
+
+# with open('data.txt', 'r', encoding='UTF-8') as file:
+#     content = file.readlines()
+#     print(content)
+#     print(type(content))
+
+import csv
+
+#
+# with open('movies.csv', 'r', encoding='UTF-8') as file:
+#     content = csv.reader(file)
+#     print(type(content))
+#     for row in content:
+#         print(type(row))  # Lista tipus
+#         print(row)  # Lista soronkent
+#         print(row[0])  # lista elemhez hozza tudunk ferni by index
+
+
+with open('movies2.csv', 'r', encoding='UTF-8') as file:
+    content = csv.reader(file, delimiter=';')
+    print(type(content))
+    for row in content:
+        print(row)
