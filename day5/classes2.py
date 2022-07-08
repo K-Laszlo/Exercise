f7f4a9f3d537414053746a55d6629dcbccdddebe
Szabo Kristof <kristof89szabo@gmail.com>
2022-07-08 09:31:01 +0200

    classes2.py

---
+++ day5/classes2.py
@@ -0,0 +1,19 @@
+class Person:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.active = True
+
+    def walk(self, email):
+        print('I am walking', self.name, self.age, email)
+
+
+person1 = Person('Krisztina', 20)
+person2 = Person('John', 20)
+
+print(person1)
+print(person2)
+# Person('John', 45)  # garbage collector ezt el fogja takaritani egy ido utan.
+#
+person1.walk('a@gmail.com')
